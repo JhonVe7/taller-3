9f8715677815d7c7ee21e6b7f5c6551e9e5b3d44
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2017-11-28 16:17:18 -0500

    Update segundo.java

--- segundo.java
+++ segundo.java
@@ -75,7 +75,7 @@ public class segundo {
 		limite -=1;
 		}
 	}
-
+	// Main 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
